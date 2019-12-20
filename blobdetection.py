bc0794458121b5a59a43953f2e17d72438d71643
rajan3010 <rajan30123010@gmail.com>
2019-12-20 06:01:16 +0530

    rest updated

---
+++ blobdetection.py
@@ -0,0 +1,16 @@
+# Created by rajan at 02/12/19
+
+import numpy as np
+import cv2
+import ImageProcUtils as ip
+import math
+
+test_image=cv2.imread('/home/rajan/DIS/HW1/blob.jpg', -1)
+test_image_gray=cv2.cvtColor(test_image,cv2.COLOR_BGR2GRAY)
+
+initial_scale=(1/math.sqrt(2))
+#initial_scale=3
+k=math.sqrt(2)
+#k=2
+
+output=ip.laplacianscale(test_image_gray, k, initial_scale)
